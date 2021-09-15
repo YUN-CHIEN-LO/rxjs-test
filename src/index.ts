f16852a466c6b799f59ad37791adf0ff7daa86fd
chien.lo <chien.lo@staff.nueip.com>
2021-09-15 14:42:06 +0800

    [Training] Drag / Drop 物件

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
-import { fromEvent } from 'rxjs';
-import { throttleTime, map, scan } from 'rxjs/operators';
+import { fromEvent } from "rxjs";
+import { map, takeUntil, concatAll } from "rxjs/operators";
 
-fromEvent(document, 'click')
-  .pipe(
-    throttleTime(1000),
-    map((event: MouseEvent) => event.clientX),
-    scan((count, clientX) => count + clientX, 0)
-  )
-  .subscribe(count => console.log(count));
+const dragDOM = document.getElementById("drag");
+const body = document.body;
+
+let mouseDown = fromEvent(dragDOM, "mousedown");
+let mouseUp = fromEvent(dragDOM, "mouseup");
+let mouseMove = fromEvent(body, "mousemove");
+
+const source = mouseDown.pipe(
+  map((event: any) => mouseMove.pipe(takeUntil(mouseUp))),
+  concatAll(),
+  map((event: any) => {
+    return {
+      x: event.clientX,
+      y: event.clientY,
+    };
+  })
+);
+source.subscribe((pos) => {
+  dragDOM.style.left = pos.x + "px";
+  dragDOM.style.top = pos.y + "px";
+});
+
+let subscriber = {
+  next: (val: any) => {
+    console.log(val);
+  },
+  complete: () => {
+    console.log("complete");
+  },
+  error: (error: any) => {
+    console.log(error);
+  },
+};
